971b9b9e686462b111c499cde3aaef0ecf2294ed
cfpryor <cfpryor@ucsc.edu>
2019-04-29 09:39:34 -0700

    Updated gathering process

--- scripts/gather_tweets.py
+++ scripts/gather_tweets.py
@@ -16,6 +16,7 @@ CACHE_PATH = os.path.join(DATA_PATH, "cache")
 # File Paths Keys
 KEYWORD_PATH = os.path.join(PRIVATE_FILES_PATH, "keywords.txt")
 TWITTER_KEYS_PATH = os.path.join(PRIVATE_FILES_PATH, "twitter_keys.json")
+CURRENT_ID_PATH = os.path.join(PRIVATE_FILES_PATH, "current_id_path.json")
 
 # Tweet Gathering Rate
 TWEET_MAX = 17000
@@ -54,7 +55,7 @@ def my_callback_closure(current_ts_instance):
             sys.stdout.flush()
             time.sleep(WAIT_TIME)
 
-def query_data(keywords, count = DEFAULT_COUNT, or_oper = True, start = None, stop = None, result_type = None, language = None, entities = None):
+def query_data(keywords, count = DEFAULT_COUNT, since_id = None, or_oper = True, start = None, stop = None, result_type = None, language = None, entities = None):
     if not os.path.isfile(TWITTER_KEYS_PATH):
         print("Twitter Keys Does Not Exist: %s" % (TWITTER_KEYS_PATH))
         sys.exit(1)
@@ -76,6 +77,8 @@ def query_data(keywords, count = DEFAULT_COUNT, or_oper = True, start = None, st
             tso.set_until(stop)
         if entities != None:
             tso.set_include_entities(entities)
+        if since_id != None:
+            tso.set_since_id(since_id)
 
         ts = TwitterSearch(
             consumer_key = twitter_keys["consumer_key"],
@@ -83,7 +86,7 @@ def query_data(keywords, count = DEFAULT_COUNT, or_oper = True, start = None, st
             access_token = twitter_keys["access_token"],
             access_token_secret = twitter_keys["access_token_secret"],
         )
-        
+
         print("Starting Query. Key: %s\tQuery URL: %s" % (keywords[0], tso.create_search_url()))
         sys.stdout.flush()
 
@@ -97,12 +100,63 @@ def query_data(keywords, count = DEFAULT_COUNT, or_oper = True, start = None, st
         print(e)
         sys.exit(1)
 
+def load_current_id():
+    # If the last id is stored use it
+    if os.path.isfile(CURRENT_ID_PATH):
+        with open(CURRENT_ID_PATH, 'r') as file:
+            return json.load(file)[-1]
+    
+    # Find the most recent id otherwise
+    id_list = []
+    for filename in os.listdir(CACHE_PATH):
+        with open(os.path.join(CACHE_PATH, filename), 'r') as file:
+            data = json.load(file)
+            for key in data:
+                id_list.append(int(key))
+    
+    # Store the most recent id
+    with open(CURRENT_ID_PATH, 'w') as file:
+        json.dump([max(id_list)], file, indent=4)
+    
+    return max(id_list)
+
 def load_keywords(path):
     keyword_list = []
     with open(path, 'r') as file:
         for line in file:
             keyword_list.append([line.strip()])
     return keyword_list
+
+def continuous_tweet_collection(keyword_path = KEYWORD_PATH, tweet_max = TWEET_MAX, query_max = QUERY_MAX, count = DEFAULT_COUNT, wait_time = WAIT_TIME):
+    # Initiate the times to starting value
+    keywords = load_keywords(keyword_path)
+    current_id = load_current_id()
+    query_count = 0
+
+    while(True):
+        id_list = []
+        for keyword in keywords:
+            query_dict = query_data(keyword, since_id = current_id)
+            for key in query_dict:
+                id_list.append(int(key))
+
+            print("Writing. Tweet Count: %d\tKey: %s\tCurrent time: %s" % (len(query_dict), keyword[0], str(datetime.datetime.now())))
+            write_query(query_dict, os.path.join(CACHE_PATH, re.sub('[#@\- ]', '', keyword[0]) + "_" + str(datetime.datetime.now().strftime("%s")) + ".txt"))
+
+            query_count += (int(len(query_dict) / count) + 1) % QUERIES_PER_SLEEP
+            if query_count >= QUERIES_PER_SLEEP:
+                print("Waiting for %d seconds." % (WAIT_TIME))
+                sys.stdout.flush()
+                time.sleep(wait_time)
+                query_count = query_count - QUERIES_PER_SLEEP
+        
+        with open(CURRENT_ID_PATH, 'r') as file:
+            curr_id_data = json.load(file)
+        with open(CURRENT_ID_PATH, 'w') as file:
+            curr_id_data.append(max(id_list))
+            json.dump(curr_id_data, file, indent = 4)
+        break
+    return
 
 def past_tweet_collection(keyword_path = KEYWORD_PATH, tweet_max = TWEET_MAX, query_max = QUERY_MAX, lookback = DEFAULT_LOOKBACK, count = DEFAULT_COUNT, wait_time = WAIT_TIME):
     # Initiate the times to starting value
@@ -130,4 +184,5 @@ def past_tweet_collection(keyword_path = KEYWORD_PATH, tweet_max = TWEET_MAX, qu
                 query_count = query_count - QUERIES_PER_SLEEP
 
 if (__name__ == '__main__'):
-    past_tweet_collection()
+    continuous_tweet_collection()
+    #past_tweet_collection()
